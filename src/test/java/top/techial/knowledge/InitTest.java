4a3c392ae195ea852732818ac99dea889d8db3bb
techial <me@techial.top>
2019-10-06 21:56:34 +0800

    add huDongItems
    Signed-off-by: techial <me@techial.top>

--- src/test/java/top/techial/knowledge/InitTest.java
+++ src/test/java/top/techial/knowledge/InitTest.java
@@ -9,7 +9,6 @@ import top.techial.knowledge.domain.HuDongItem;
 import top.techial.knowledge.utils.ResourceUtils;
 
 import java.util.function.Function;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @SpringBootTest
@@ -22,26 +21,22 @@ public class InitTest {
     private static final Function<String, HuDongItem> HU_DONG_ITEM_FUNCTION = InitTest::apply;
 
     private static HuDongItem apply(String key) {
-        Matcher matcher = PATTERN.matcher(key);
-        if (matcher.find()) {
-            log.info(matcher.group());
-            return new HuDongItem()
-                .setTitle(matcher.group(1))
-                .setUrl(matcher.group(2))
-                .setImage(matcher.group(3))
-                .setOpenTypeList(matcher.group(4))
-                .setDetail(matcher.group(5))
-                .setBaseInfoKeyList(matcher.group(6))
-                .setBaseInfoValueList(matcher.group(7));
-        }
-        return null;
+        String[] strings = key.split(",");
+        return new HuDongItem()
+            .setTitle(strings[0])
+            .setBaseInfoValueList(strings[1])
+            .setBaseInfoKeyList(strings[2]);
+    }
+
+    @Test
+    public void test() {
+
     }
 
 
     @Test
     public void importTest() {
-        ResourceUtils.read("data/hu-dong.csv", HU_DONG_ITEM_FUNCTION)
-            .forEachRemaining(System.out::println);
+        ResourceUtils.read("data/hudong_pedia.csv", HU_DONG_ITEM_FUNCTION);
     }
 
 }
