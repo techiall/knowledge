33f134d725f06c8b83bac0898c7203723bc2b140
techial <me@techial.top>
2019-10-28 23:19:37 +0800

    add cache
    Signed-off-by: techial <me@techial.top>

---
+++ api/src/main/java/top/techial/knowledge/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package top.techial.knowledge.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author techial
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
