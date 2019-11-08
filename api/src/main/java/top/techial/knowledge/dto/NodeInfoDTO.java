07cd56a7ca0889ae6a4100fd489cd7cc87608546
techial <me@techial.top>
2019-11-08 10:23:23 +0800

    fix
    Signed-off-by: techial <me@techial.top>

---
+++ api/src/main/java/top/techial/knowledge/dto/NodeInfoDTO.java
@@ -0,0 +1,34 @@
+package top.techial.knowledge.dto;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import top.techial.knowledge.domain.KnowledgeNode;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author techial
+ */
+@Data
+@Accessors(chain = true)
+public class NodeInfoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public NodeInfoDTO(KnowledgeNode knowledgeNode) {
+        this.id = knowledgeNode.getId();
+        this.labels = knowledgeNode.getLabels();
+        this.name = knowledgeNode.getName();
+        this.property = knowledgeNode.getProperty();
+    }
+
+    private Long id;
+
+    private Set<String> labels;
+
+    private String name;
+
+    private Map<String, String> property;
+
+}
