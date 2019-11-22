07cdb78b6f64003f6166dc416d33d10cda6787f6
techial <me@techial.top>
2019-11-22 12:06:32 +0800

    fix
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,6 @@
 git fetch && \
 git reset --hard origin/master
 
-mvn clean install package docker:build -DskipTests=true
+./mvnw clean install package docker:build -DskipTests=true
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
