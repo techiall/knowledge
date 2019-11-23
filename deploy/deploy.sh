78a1c6a32be6849ec1cba158ba9abb8fabe468dc
techial <me@techial.top>
2019-11-23 16:12:17 +0800

    update config
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,8 +3,8 @@
 git fetch && \
 git reset --hard origin/master
 
-./mvnw clean install package -DskipTests=true
-cd ./api
-../mvnw clean install package jib:dockerBuild -DskipTests=true
+./mvnw clean install -DskipTests=true && \
+cd ./api && \
+../mvnw clean compile jib:dockerBuild -DskipTests=true && \
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
