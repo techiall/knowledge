b704614dc12cec6a7b260be80534ec183f3945c5
techial <me@techial.top>
2019-12-06 14:37:45 +0800

    polish
    Signed-off-by: techial <me@techial.top>

--- api/src/main/java/top/techial/knowledge/service/RecordService.java
+++ api/src/main/java/top/techial/knowledge/service/RecordService.java
@@ -5,6 +5,7 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import top.techial.knowledge.dao.RecordRepository;
 import top.techial.knowledge.domain.Record;
@@ -21,26 +22,31 @@ public class RecordService {
         this.recordRepository = recordRepository;
     }
 
+    @Async
     @CacheEvict(allEntries = true)
     public Record save(Record record) {
         return recordRepository.save(record);
     }
 
+    @Async
     @Cacheable(key = "#root.targetClass.simpleName + #root.methodName + #pageable", unless = "#result == null")
     public Page<Record> findAll(Pageable pageable) {
         return recordRepository.findAll(pageable);
     }
 
+    @Async
     @Cacheable(key = "#root.targetClass.simpleName + #root.methodName + #id + #pageable", unless = "#result == null")
     public Page<Record> findByNodeId(Long id, Pageable pageable) {
         return recordRepository.findByNodeId(id, pageable);
     }
 
     @CacheEvict(allEntries = true)
+    @Async
     public void deleteByNodeId(Long id) {
         recordRepository.deleteByNodeId(id);
     }
 
+    @Async
     @CacheEvict(allEntries = true)
     public void deleteAll() {
         recordRepository.deleteAll();
