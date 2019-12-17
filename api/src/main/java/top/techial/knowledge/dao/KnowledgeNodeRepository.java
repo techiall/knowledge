e364f4e12243a20c5dbea6d31b3359b30241ad29
techial <me@techial.top>
2019-12-17 15:39:08 +0800

    fix
    Signed-off-by: techial <me@techial.top>

--- api/src/main/java/top/techial/knowledge/dao/KnowledgeNodeRepository.java
+++ api/src/main/java/top/techial/knowledge/dao/KnowledgeNodeRepository.java
@@ -3,7 +3,6 @@ package top.techial.knowledge.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import top.techial.knowledge.domain.KnowledgeNode;
 
@@ -16,7 +15,7 @@ import java.util.Optional;
 public interface KnowledgeNodeRepository extends Neo4jRepository<KnowledgeNode, Long> {
     Page<KnowledgeNode> findByNameLikeAndUserId(String name, String userId, Pageable pageable);
 
-    Page<KnowledgeNode> findByUserIdAndParentNodeIdIsNull(@Param("userId") String userId, Pageable pageable, int depth);
+    Page<KnowledgeNode> findByUserIdAndParentNodeIdIsNull(String userId, Pageable pageable, int depth);
 
     Optional<KnowledgeNode> findFirstByName(String name);
 
