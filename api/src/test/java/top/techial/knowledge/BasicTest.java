2a4f2bb6cac0f7c5d6fac1560f83e81d2e4f6dcd
techial <me@techial.top>
2020-01-17 18:37:24 +0800

    add BasicTest
    Signed-off-by: techial <me@techial.top>

---
+++ api/src/test/java/top/techial/knowledge/BasicTest.java
@@ -0,0 +1,20 @@
+package top.techial.knowledge;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+/**
+ * @author techial
+ */
+public abstract class BasicTest<T> {
+
+    protected static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    public abstract T newInstance();
+}
---
+++ api/src/test/java/top/techial/knowledge/dto/NodeBaseDTOTest.java
@@ -0,0 +1,11 @@
+package top.techial.knowledge.dto;
+
+import top.techial.knowledge.BasicTest;
+
+public class NodeBaseDTOTest extends BasicTest<NodeBaseDTO> {
+
+    @Override
+    public NodeBaseDTO newInstance() {
+        return null;
+    }
+}
