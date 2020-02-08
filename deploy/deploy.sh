fa309fc3817af4138604e72ab951673eef920cdf
techial <me@techial.top>
2020-02-08 22:35:54 +0800

    Add github action
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,7 @@
 git fetch && \
 git reset --hard origin/master
 
+chmod +x ./gradlew
 ./gradlew clean jibDockerBuild && \
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
