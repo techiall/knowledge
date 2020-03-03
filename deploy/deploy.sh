444209af6425e06716fd830b6c0c04174ddbb1af
techial <me@techial.top>
2020-03-03 18:21:47 +0800

    fix
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,9 @@ git fetch && \
 git reset --hard origin/master
 
 chmod +x ./gradlew
-./gradlew clean jibDockerBuild && \
+./gradlew clean && \
+yarn install && yarn build && \
+./gradlew jibDockerBuild && \
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
