a299ac8ed51b415cb300816153054181c7df24e7
techial <me@techial.top>
2020-03-04 09:38:47 +0800

    update deploy.sh
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,10 +3,8 @@
 git fetch && \
 git reset --hard origin/master
 
-chmod +x ./gradlew
-./gradlew clean && \
-yarn install && yarn build && \
-./gradlew jibDockerBuild && \
+chmod +x ./gradlew && \
+./gradlew clean yarnBuild jibDockerBuild
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
