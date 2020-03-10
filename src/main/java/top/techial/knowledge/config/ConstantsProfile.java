34cbc19f9c597edc85889cbc3566559beedbac53
techial <me@techial.top>
2020-03-10 10:55:14 +0800

    polish
    Signed-off-by: techial <me@techial.top>

--- src/main/java/top/techial/knowledge/config/ConstantsProfile.java
+++ src/main/java/top/techial/knowledge/config/ConstantsProfile.java
@@ -5,7 +5,7 @@ package top.techial.knowledge.config;
  */
 public final class ConstantsProfile {
     public static final String DEV = "dev";
-    public static final String PRO = "pro";
+    public static final String PROD = "prod";
     public static final String TEST = "test";
 
     private ConstantsProfile() {
