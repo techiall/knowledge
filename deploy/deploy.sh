3d7e439266db3e60cd884ba78a1923300a0f3588
techial <me@techial.top>
2020-03-10 12:20:31 +0800

    fix workflows
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,10 @@ git reset --hard origin/master
 
 chmod +x ./gradlew && \
 ./gradlew clean yarnBuild jibDockerBuild
+
+docker volume create --name=redis_data
+docker volume create --name=mysql_data
+
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
 
--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,10 @@
 const path = require('path')
 
 const resolve = dir => {
-  return path.join(__dirname, dir)
+    return path.join(__dirname, dir)
 }
 
 module.exports = {
-<<<<<<< HEAD
-  outputDir: './build/resources/main/static',
-  devServer: {
-    proxy: {
-      '/api': {
-        target: "https://knowledge.pchelper666.com/",
-        changeOrigin: true,
-      }
-    }
-  },
-  configureWebpack: config => {
-    config.externals = {
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      'vuex': 'Vuex',
-      'axios': 'axios',
-      'jquery': '$',
-      'echarts': 'echarts'
-=======
     chainWebpack: config => {
         config.plugins.delete('prefetch');
     },
@@ -58,24 +39,5 @@ module.exports = {
             // 在 dist/index.html 的输出
             filename: 'index.html',
         },
->>>>>>> 70677c27101deb7aca0e37f8482053118da734a0
     }
-    config.entry.app = './src/main/webapp/src/main.js';
-    config.resolve.alias = {
-      '@': resolve('./src/main/webapp/src')
-    }
-  },
-  chainWebpack: config => {
-    // 移除 prefetch 插件
-    config.plugins.delete('prefetch');
-  },
-  pages: {
-    index: {
-      entry: './src/main/webapp/src/main.js',
-      template: './src/main/webapp/public/index.html',
-      favicon: './src/main/webapp/public/favicon.ico',
-      // 在 dist/index.html 的输出
-      filename: 'index.html',
-    },
-  }
 };
