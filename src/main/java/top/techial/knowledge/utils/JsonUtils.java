a09297e53562d878f03ac1ef6bde05ea3ed5fda8
techial <me@techial.top>
2020-03-14 23:50:52 +0800

    fix
    Signed-off-by: techial <me@techial.top>

--- src/main/java/top/techial/knowledge/utils/JsonUtils.java
+++ src/main/java/top/techial/knowledge/utils/JsonUtils.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 /**
  * @author techial
@@ -29,7 +30,8 @@ public class JsonUtils {
         return "";
     }
 
-    public static <T> T readValue(ObjectMapper objectMapper, String str, Class<T> aClass) {
+    @Nullable
+    public static <T> T readValue(@NonNull ObjectMapper objectMapper, @NonNull String str, @NonNull Class<T> aClass) {
         try {
             return objectMapper.readValue(str, aClass);
         } catch (JsonProcessingException e) {
