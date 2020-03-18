9df564469c3a6f62ada13fab3d24b5febec25fb8
techial <me@techial.top>
2020-03-18 17:48:28 +0800

    add ElasticsearchConfiguration
    Signed-off-by: techial <me@techial.top>

---
+++ src/main/java/top/techial/knowledge/config/ElasticsearchConfiguration.java
@@ -0,0 +1,98 @@
+package top.techial.knowledge.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.vanroy.springdata.jest.JestElasticsearchTemplate;
+import com.github.vanroy.springdata.jest.mapper.DefaultJestResultsMapper;
+import io.searchbox.client.JestClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.EntityMapper;
+import org.springframework.data.elasticsearch.core.convert.ElasticsearchConverter;
+import org.springframework.data.elasticsearch.core.mapping.SimpleElasticsearchMappingContext;
+import org.springframework.data.mapping.MappingException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author techial
+ */
+@Configuration
+@EnableConfigurationProperties(ElasticsearchProperties.class)
+public class ElasticsearchConfiguration {
+
+    private final ObjectMapper objectMapper;
+
+    public ElasticsearchConfiguration(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    public EntityMapper getEntityMapper() {
+        return new CustomEntityMapper(objectMapper);
+    }
+
+    @Bean
+    @Primary
+    public ElasticsearchOperations elasticsearchTemplate(
+            final JestClient jestClient,
+            final ElasticsearchConverter elasticsearchConverter,
+            final SimpleElasticsearchMappingContext simpleElasticsearchMappingContext,
+            @Qualifier("getEntityMapper") EntityMapper mapper
+    ) {
+        return new JestElasticsearchTemplate(
+                jestClient,
+                elasticsearchConverter,
+                new DefaultJestResultsMapper(simpleElasticsearchMappingContext, mapper));
+    }
+
+    public static class CustomEntityMapper implements EntityMapper {
+
+        private final ObjectMapper objectMapper;
+
+        public CustomEntityMapper(ObjectMapper objectMapper) {
+            this.objectMapper = objectMapper;
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+            objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
+            objectMapper.configure(SerializationFeature.INDENT_OUTPUT, false);
+            objectMapper.configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
+        }
+
+        @Override
+        public String mapToString(Object object) throws IOException {
+            return objectMapper.writeValueAsString(object);
+        }
+
+        @Override
+        public <T> T mapToObject(String source, Class<T> clazz) throws IOException {
+            return objectMapper.readValue(source, clazz);
+        }
+
+        @Override
+        public Map<String, Object> mapObject(Object source) {
+            try {
+                return objectMapper.readValue(mapToString(source), HashMap.class);
+            } catch (IOException e) {
+                throw new MappingException(e.getMessage(), e);
+            }
+        }
+
+        @Override
+        public <T> T readObject(Map<String, Object> source, Class<T> targetType) {
+            try {
+                return mapToObject(mapToString(source), targetType);
+            } catch (IOException e) {
+                throw new MappingException(e.getMessage(), e);
+            }
+        }
+    }
+}
