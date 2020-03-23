87deb0758423bd16fbca4d910be3c933c51fcbe5
techial <me@techial.top>
2020-03-23 15:38:13 +0800

    polish
    Signed-off-by: techial <me@techial.top>

--- src/main/java/top/techial/knowledge/config/CacheConfiguration.java
+++ src/main/java/top/techial/knowledge/config/CacheConfiguration.java
@@ -2,8 +2,10 @@ package top.techial.knowledge.config;
 
 import org.hibernate.cache.jcache.ConfigSettings;
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.jcache.configuration.RedissonConfiguration;
+import org.redisson.spring.session.config.EnableRedissonHttpSession;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
@@ -21,6 +23,7 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
+@EnableRedissonHttpSession
 public class CacheConfiguration {
     private final RedisProperties redisProperties;
 
@@ -29,13 +32,19 @@ public class CacheConfiguration {
     }
 
     @Bean
-    public javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration() {
-        MutableConfiguration<Object, Object> jcacheConfig = new MutableConfiguration<>();
+    public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer().setAddress(redisProperties.getHost());
+        config.useSingleServer().setDatabase(redisProperties.getDatabase());
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration() {
+        MutableConfiguration<Object, Object> jcacheConfig = new MutableConfiguration<>();
         jcacheConfig.setStatisticsEnabled(true);
         jcacheConfig.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, 3600)));
-        return RedissonConfiguration.fromInstance(Redisson.create(config), jcacheConfig);
+        return RedissonConfiguration.fromInstance(redissonClient(), jcacheConfig);
     }
 
 
