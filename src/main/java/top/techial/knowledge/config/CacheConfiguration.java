5f407e6cd7fb2076db8f3caa721c8cb62f01b6a1
techial <me@techial.top>
2020-03-23 15:01:51 +0800

    Add Hibernate h2 cache
    Signed-off-by: techial <me@techial.top>

---
+++ src/main/java/top/techial/knowledge/config/CacheConfiguration.java
@@ -0,0 +1,22 @@
+package top.techial.knowledge.config;
+
+import org.hibernate.cache.jcache.ConfigSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author techial
+ */
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    @Bean
+    public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(javax.cache.CacheManager cm) {
+        return hibernateProperties -> hibernateProperties.put(ConfigSettings.CACHE_MANAGER, cm);
+    }
+
+}
