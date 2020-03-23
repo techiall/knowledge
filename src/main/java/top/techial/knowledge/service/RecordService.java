8e5b9bd23032366c1e70be8f16df6385e22c17c3
techial <me@techial.top>
2020-03-23 09:59:05 +0800

    fix org.hibernate.TransientPropertyValueException
    Signed-off-by: techial <me@techial.top>

--- src/main/java/top/techial/knowledge/service/RecordService.java
+++ src/main/java/top/techial/knowledge/service/RecordService.java
@@ -1,11 +1,9 @@
 package top.techial.knowledge.service;
 
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import top.techial.knowledge.domain.Record;
-import top.techial.knowledge.domain.User;
 import top.techial.knowledge.repository.RecordRepository;
+import top.techial.knowledge.repository.UserRepository;
 
 /**
  * @author techial
@@ -13,19 +11,19 @@ import top.techial.knowledge.repository.RecordRepository;
 @Service
 public class RecordService {
     private final RecordRepository recordRepository;
+    private final UserRepository userRepository;
 
-    public RecordService(RecordRepository recordRepository) {
+    public RecordService(RecordRepository recordRepository, UserRepository userRepository) {
         this.recordRepository = recordRepository;
+        this.userRepository = userRepository;
     }
 
-    @Async
-    @Transactional
     public void save(Long nodeId, Integer userId, String operatorMessage, String message) {
         Record record = new Record()
                 .setNodeId(nodeId)
                 .setOperator(operatorMessage)
                 .setMessage(message)
-                .setUser(new User().setId(userId));
+                .setUser(userRepository.getOne(userId));
         recordRepository.save(record);
     }
 
