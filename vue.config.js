5db744226f0355f60bcd61ae19a4afd364ea000c
pbove <771757562@qq.com>
2020-03-28 13:52:42 +0800

    fix load webpack

--- vue.config.js
+++ vue.config.js
@@ -1,47 +1,48 @@
-const path = require('path')
+const path = require('path');
 
-const resolve = dir => {
-    return path.join(__dirname, dir)
-}
+const resolve = (dir) => {
+  return path.join(__dirname, dir);
+};
 
 module.exports = {
-    chainWebpack: config => {
-        config.plugins.delete('prefetch');
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch');
+  },
+  outputDir: './build/resources/main/static',
+  productionSourceMap: false,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://knowledge.pchelper666.com/',
+        changeOrigin: true,
+      },
+      '/static': {
+        target: 'https://knowledge.pchelper666.com/',
+        changeOrigin: true,
+      },
     },
-    outputDir: './build/resources/main/static',
-    devServer: {
-        proxy: {
-            '/api': {
-                target: "https://knowledge.pchelper666.com/",
-                changeOrigin: true,
-            },
-            '/static': {
-                target: "https://knowledge.pchelper666.com/",
-                changeOrigin: true,
-            }
-        }
+  },
+  configureWebpack: (config) => {
+    config.externals = {
+      vue: 'Vue',
+      'vue-router': 'VueRouter',
+      vuex: 'Vuex',
+      axios: 'axios',
+      jquery: '$',
+      echarts: 'echarts',
+    };
+    config.entry.app = './src/main/webapp/src/main.js';
+    config.resolve.alias = {
+      '@': resolve('./src/main/webapp/src'),
+    };
+  },
+  pages: {
+    index: {
+      entry: './src/main/webapp/src/main.js',
+      template: './src/main/webapp/public/index.html',
+      favicon: './src/main/webapp/public/favicon.ico',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
     },
-    configureWebpack: config => {
-        config.externals = {
-            vue: 'Vue',
-            'vue-router': 'VueRouter',
-            'vuex': 'Vuex',
-            'axios': 'axios',
-            'jquery': '$',
-            'echarts': 'echarts'
-        }
-        config.entry.app = './src/main/webapp/src/main.js';
-        config.resolve.alias = {
-            '@': resolve('./src/main/webapp/src')
-        }
-    },
-    pages: {
-        index: {
-            entry: './src/main/webapp/src/main.js',
-            template: './src/main/webapp/public/index.html',
-            favicon: './src/main/webapp/public/favicon.ico',
-            // 在 dist/index.html 的输出
-            filename: 'index.html',
-        },
-    }
+  },
 };
