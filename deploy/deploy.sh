8bf96f590182921c415cf8e74034a32ea3768509
techial <me@techial.top>
2020-04-30 15:04:19 +0800

    fix
    Signed-off-by: techial <me@techial.top>

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,9 +6,9 @@ git reset --hard origin/master
 chmod +x ./gradlew && \
 ./gradlew clean yarnBuild jibDockerBuild
 
-docker volume create --name=redis_data
-docker volume create --name=mysql_data
-docker volume create --name=elastic_data
+docker volume create --name=knowledge_redis_data
+docker volume create --name=knowledge_mysql_data
+docker volume create --name=knowledge_elastic_data
 
 docker-compose up -d
 docker images | awk '$1 == "<none>" || $2 == "<none>" {print $3}' | xargs docker rmi
