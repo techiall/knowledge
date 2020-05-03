918ac2bd84a10246c75398a01ca753592476001e
techial <me@techial.top>
2020-05-03 13:09:05 +0800

    polish
    Signed-off-by: techial <me@techial.top>

--- src/main/java/top/techial/knowledge/Init.java
+++ src/main/java/top/techial/knowledge/Init.java
@@ -1,24 +1,37 @@
 package top.techial.knowledge;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 import top.techial.knowledge.domain.User;
 import top.techial.knowledge.repository.UserRepository;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Component
 public class Init implements CommandLineRunner {
     private final UserRepository userRepository;
+    private final ObjectMapper objectMapper;
 
-    public Init(UserRepository userRepository) {
+    public Init(UserRepository userRepository, ObjectMapper objectMapper) {
         this.userRepository = userRepository;
+        this.objectMapper = objectMapper;
     }
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws IOException {
         if (userRepository.count() == 0L) {
-            userRepository.save(new User().setAccountNonExpired(true).setAccountNonLocked(true).setCredentialsNonExpired(true).setEnabled(true).setNickName("root").setUserName("root").setPassword("{noop}root"));
-            userRepository.save(new User().setAccountNonExpired(true).setAccountNonLocked(true).setCredentialsNonExpired(true).setEnabled(true).setNickName("admin").setUserName("admin").setPassword("{noop}admin"));
-            userRepository.save(new User().setAccountNonExpired(true).setAccountNonLocked(true).setCredentialsNonExpired(true).setEnabled(true).setNickName("techial").setUserName("techial").setPassword("{noop}techial"));
+            byte[] bytes = resource("data/user.json");
+            User[] users = objectMapper.readValue(bytes, User[].class);
+            userRepository.saveAll(Arrays.asList(users));
         }
+    }
+
+    private static byte[] resource(String path) throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource(path);
+        return FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
     }
 }
