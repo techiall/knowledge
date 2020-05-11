38b0a82163a224d7cd93fc0f5f01ba32abb93fa1
techial <me@techial.top>
2020-05-11 11:18:06 +0800

    add ETagConfig
    Signed-off-by: techial <me@techial.top>

---
+++ src/main/java/top/techial/knowledge/config/ETagConfig.java
@@ -0,0 +1,17 @@
+package top.techial.knowledge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+/**
+ * @author techial
+ */
+@Configuration
+public class ETagConfig {
+
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+}
